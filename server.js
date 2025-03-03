f3ded756f6ac96d2c7f94dd33713cdf28ada0dd1
RagavendranRj05 <rj05ragavendran@gmail.com>
2025-03-03 14:11:37 +0530

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 3001;
 
 mongoose
-  .connect("mongodb+srv://votingverficationmachine:VVM%40123*@cluster0.nr8cn.mongodb.net/VVMDB")
+  .connect("mongodb+srv://votingverficationmachine:VVMvoting2030@cluster0.nr8cn.mongodb.net/VVMDB")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
