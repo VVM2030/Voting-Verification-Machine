261f9ab89f1e134d4c08d4b113f343fc05a3177e
RagavendranRj05 <rj05ragavendran@gmail.com>
2025-03-03 13:36:44 +0530

    first commit

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ require("dotenv").config();
 const app = express();
 const port = 3001;
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/VVMDB")
+mongoose.connect("mongodb://127.0.0.1:27017/VVMDB", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
